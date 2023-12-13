3534ead30e818fbb0803bd03ac45456f5e27d09d
moses-ga <ganzamoses44@gmail.com>
2023-12-13 13:39:47 -0800

    update

--- get_env.c
+++ get_env.c
@@ -18,58 +18,46 @@ char *get_env(const char *name)
 	char *variable, *value, *path;
 	int compare;
 	unsigned int pa_length, env_length, length, i;
-	/* Count the number of elements in the environ array */
+
 	env_length = 0;
 	while (environ[env_length] != NULL)
 		env_length++;
-	/* Create a copy of the environment variables */
 	env_copy = NULL;
 	env_copy = copy_env(env_copy, env_length);
-	/* Calculate the length of the input environment variable name */
 	length = str_len((char *)name);
 	i = 0;
-	/* Loop through the copied environment variables */
 	while (env_copy[i] != NULL)
 	{
 		variable = env_copy[i];
-		/* Compare the input name with the current environment variable */
 		compare = str_ncmp((char *)name, variable, length);
-		/* If there is a match */
 		if (compare == 1)
 		{
-			/* Extract the value part of the environment variable */
 			value = strtok(variable, "=");
 			value = strtok(NULL, "\n ");
-			/* Handle the case where the value is NULL */
 			if (value == NULL)
 			{
 				errors(4);
 				exit(EXIT_FAILURE);
 			}
-			/* Calculate the length of the value */
 			pa_length = str_len(value);
-			/* Allocate memory for the path variable */
 			path = malloc(sizeof(char) * pa_length + 1);
 			if (path == NULL)
 			{
 				errors(3);
 				return (NULL);
 			}
-			/* Copy the value to the newly allocated memory */
 			path = str_cpy(path, value);
-			/* Free the copied environment variables */
 			free_dp(env_copy, env_length);
-			/* Return the path variable */
 			return (path);
 		}
 		i++;
 	}
-	/* If no match is found, return NULL */
 	return (NULL);
 }
 
 /**
- * copy_env - This function creates a copy of the environment variables to avoid
+ * copy_env - This function creates a copy of the environment
+ * variables to avoid
  * modifying the original environment.
  * It allocates memory for the copy and each individual environment variable.
  * It uses str_cpy to copy each environment variable to the new memory.
@@ -83,7 +71,7 @@ char **copy_env(char **env_copy, unsigned int env_length)
 	char *variable;
 	unsigned int variable_length;
 	unsigned int i;
-	/* Allocate memory for the copy of environment variables */
+
 	env_copy = malloc(sizeof(char **) * (env_length));
 	if (env_copy == NULL)
 	{
@@ -91,24 +79,20 @@ char **copy_env(char **env_copy, unsigned int env_length)
 		return (NULL);
 	}
 	i = 0;
-	/* Loop through the original environment variables */
+
 	while (i < env_length)
 	{
 		variable = environ[i];
-		/* Calculate the length of the current environment variable */
 		variable_length = str_len(variable);
-		/* Allocate memory for the copy of the current environment variable */
 		env_copy[i] = malloc(sizeof(char) * variable_length + 1);
 		if (env_copy[i] == NULL)
 		{
 			errors(3);
 			return (NULL);
 		}
-		/* Copy the original environment variable to the copy */
 		str_cpy(env_copy[i], environ[i]);
 		i++;
 	}
-	/* Return the copy of environment variables */
 	return (env_copy);
 }
 
--- main.c
+++ main.c
@@ -33,64 +33,46 @@ int main(void)
 {
 	char *line, *path, *fullpath;
 	char **tokens;
-	int flag = 0; /* 0 if fullpath is not malloc'd, 1 otherwise */
+	int flag = 0;
 	int builtin_status, child_status;
 	struct stat buf;
-	/* Main shell loop */
+
 	while (TRUE)
 	{
-		/* Display the shell prompt and get user input */
 		prompt(STDIN_FILENO, buf);
 		line = get_line(stdin);
-		/* Check if the input line is empty (just Enter) */
 		if (str_cmp(line, "\n", 1) == 0)
 		{
-			free(line);
-			continue; /* Skip to the next iteration if the line is empty */
+		free(line);
+		continue;
 		}
-		/* Tokenize the input line into individual commands and arguments */
 		tokens = tokenizer(line);
-		/* If the first token is NULL, meaning an empty line, */
-		/* skip to next iteration */
 		if (tokens[0] == NULL)
 		{
 			free(tokens);
 			continue;
 		}
-		/* Execute built-in commands (e.g., exit) or continue */
-		/*to external command execution */
 		builtin_status = buiilt_in_execu(tokens);
-		/* Free memory allocated for tokens input line if necessary */
 		if (builtin_status == 0 || builtin_status == -1)
 		{
 			free(tokens);
 			free(line);
 		}
-		/* If built-in command was successful or user reques exit */
-		/* continue to the next iteration */
 		if (builtin_status == 0)
 			continue;
-		/* If the user requested exit, terminate the shell */
 		if (builtin_status == -1)
 			_exit(EXIT_SUCCESS);
-		/* Get the value of the PATH environment variable */
 		path = get_env("PATH");
-		/* Determine the full path of the command to be executed */
 		fullpath = which_path(tokens[0], fullpath, path);
-		/* If fullpath is NULL, use the entered command as is */
 		if (fullpath == NULL)
 			fullpath = tokens[0];
 		else
-			flag = 1; /* flag to 1 indicat fullpath was allocat */
-		/* Execute the command in a child process */
+			flag = 1;
 		child_status = child(fullpath, tokens);
-		/* Display an error if the child process fails to execute */
 		if (child_status == -1)
 			errors(2);
-		/* Free allocated memory for tokens, the PATH variable, the */
-		/* input line, and fullpath if it was dynamically allocated */
 		free_all(tokens, path, line, fullpath, flag);
 	}
-	/* Return 0 on successful completion */
 	return (0);
 }
+

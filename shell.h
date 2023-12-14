664986bad5aee07416cc1f041090129f9093a82b
moses-ga <ganzamoses44@gmail.com>
2023-12-13 16:26:04 -0800

    Simple shell

--- shell.h
+++ shell.h
@@ -12,18 +12,16 @@
 #define BUFFER 1024
 #define TRUE 1
 #define PROMPT "$ "
-/* error strings */
+
 #define ERR_MALLOC "Unable to malloc space\n"
 #define ERR_FORK "Unable to fork and create child process\n"
 #define ERR_PATH "No such file or directory\n"
 extern char **environ;
 
 /**
- * struct list_s - linked list of variables
- * @value: value
- * @next: pointer to next node
- *
- * Description: generic linked list struct for variables.
+ * struct list_s - linked listherjkfne of variables
+ * @value: valuehfnkejfv
+ * @next: pointer erhfto nextmfl node
 **/
 typedef struct list_s
 {
@@ -32,11 +30,9 @@ typedef struct list_s
 } list_s;
 
 /**
- * struct built_s - linked list of builtins
- * @name: name of builtin
- * @p: pointer to function
- *
- * Description: struct for builtin functions.
+ * struct built_s - linked lkrf,lf,ist of builrfatins
+ * @name: name ofmlwkfmf builtin
+ * @p: pointer uerto funmfakaction
 **/
 typedef struct built_s
 {
@@ -51,26 +47,24 @@ char *which_path(char *command, char *fullpath, char *path);
 int child(char *fullpath, char **tokens);
 void errors(int error);
 
-/* utility functions */
 void _puts(char *str);
 int str_len(char *s);
 int str_cmp(char *name, char *variable, unsigned int length);
 int str_ncmp(char *name, char *variable, unsigned int length);
 char *str_cpy(char *dest, char *src);
 
-/* prototypes for builtins */
 int shell_env(void);
 int shell_exit(void);
 int buiilt_in_execu(char **tokens);
 int shell_no_builtins(built_s builtin[]);
 
-/* prototypes for the helper functions for path linked list */
 char *get_env(const char *name);
 char **copy_env(char **environ_copy, unsigned int environ_length);
 list_s *pathlist(char *variable, list_s *head);
 
-/* prototypes for free functions */
+
 void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
 void free_dp(char **array, unsigned int length);
 
-#endif /* SHELL_H */
+#endif 
+

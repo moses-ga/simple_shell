39bd9f7599f3c8a494641ea0ee1243a1225da4d7
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-12-16 04:03:54 +0200

    why

--- moz1.c
+++ moz1.c
@@ -41,47 +41,51 @@ int _myexit(info_t *info)
  */
 int _mycd(info_t *info)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+    char *s, *dir, buffer[1024];
+    int chdir_ret;
 
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->arntv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret =
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->arntv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret =
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->arntv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->arntv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
+    s = getcwd(buffer, sizeof(buffer));
+    if (!s)
+        perror("getcwd");
+    
+    if (!info->arntv[1])
+    {
+        dir = _getenv(info, "HOME=");
+        if (!dir)
+            chdir_ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+        else
+            chdir_ret = chdir(dir);
+    }
+    else if (_strcmp(info->arntv[1], "-") == 0)
+    {
+        if (!_getenv(info, "OLDPWD="))
+        {
+            _puts(s);
+            _putchar('\n');
+            return (1);
+        }
+        _puts(_getenv(info, "OLDPWD="));
+        _putchar('\n');
+        chdir_ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+    }
+    else
+        chdir_ret = chdir(info->arntv[1]);
+
+    if (chdir_ret == -1)
+    {
+        perror("chdir");
+        print_error(info, "can't cd to ");
+        _eputs(info->arntv[1]);
+        _eputchar('\n');
+    }
+    else
+    {
+        _setenv(info, "OLDPWD", _getenv(info, "PWD="));
+        _setenv(info, "PWD", getcwd(buffer, sizeof(buffer)));
+    }
+    return (0);
 }
+
 
 /**
  * _myhelp - changes the currdfent direcdftory of the prodfcess

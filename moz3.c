03300e7d7eee452c527eaf16feacfc208191ecff
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-12-16 04:22:04 +0200

    trying again

--- moz3.c
+++ moz3.c
@@ -1,95 +1,99 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
+#include "shell.h"
+
 /**
- * _myenv - princvbts the currcvbcbvent environment
- * @info: Structbvcbvure containing potentibval argumencbvts.
- * Uscbved to mbvcainvtain
- * constant funccbvtion prototbvcype.
- * Return: Alwcbvays 0
+ * getEnviron - returns the string array copy of our environ
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int _myenv(info_t *info)
+ 
+ 
+char **getEnviron(info_t *info)
 {
-	print_list_str(info->evrnt);
-	return (0);
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
 }
 
 /**
- * _getenv - getcbvs the valcbvue of an envibvcron varibvable
- * @info: Structure contaicbvning potential argumbvcents. Used to maibvntain
- * @name: env vcbvar ncbvame
- *
- * Return: the valcbvue
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
  */
-char *_getenv(info_t *info, const char *name)
+ 
+ 
+int _unsetenv(info_t *info, char *var)
 {
-	list_t *node = info->evrnt;
+	list_t *node = info->env;
+	size_t i = 0;
 	char *p;
+
+	if (!node || !var)
+		return (0);
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+ 
+ 
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
 		node = node->next;
 	}
-	return (NULL);
-}
-
-/**
- * _mysetenv - Initialicbvcbvze a new envircbvonment variabbvcle,
- * or mocbvdify an existcbving one
- * @info: Structure contacbvining potcbvential argumcbvents.
- * Usedcbv to maicbvntain
- * onstant functcbvion protocbvtype.
- *  Return: Alwcbvays 0
- */
-int _mysetenv(info_t *info)
-{
-	if (info->arntc != 3)
-	{
-		_eputs("Incorrect number of arguements\n");
-		return (1);
-	}
-	if (setenv(info->arntv[0], info->arntv[1], 1))
-		return (0);
-	return (1);
-}
-
-/**
- * _myunsetenv - Remocbvve an environcbvcbvment variablbve
- * @info: Structure contcbvaining potcbvential argucbvments. Used tcbv
- * o maintain conscbvtant function procbvtotype.
- *  Return: Alwacbvys 0
- */
-int _myunsetenv(info_t *info)
-{
-	int i;
-
-	if (info->arntc == 1)
-	{
-		_eputs("Too few arguements.\n");
-		return (1);
-	}
-	for (i = 1; i <= info->arntc; i++)
-		_unsetenv(info, info->arntv[i]);
-
-	return (0);
-}
-
-/**
- * populate_env_list - populatecbvs env linkecbvd list
- * @info: Structure concbvcbvtaining potential argucbvments.
- * Used to maicbvntaincbvconcbvstcbvant function prvbototype.
- * Return: Alwacbvys 0
- */
-int populate_env_list(info_t *info)
-{
-	list_t *node = NULL;
-	size_t i;
-
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->evrnt = node;
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
 	return (0);
 }
